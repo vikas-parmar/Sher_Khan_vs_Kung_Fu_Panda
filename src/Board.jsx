932ac10eaac24e7414149c477165ef2e3e5b582f
Vikas Panwar <vkparmar003@gmail.com>
2023-03-07 19:12:09 +0530

    adding squares into Board component

---
+++ src/Board.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Square from './Square'
+
+const Board = () => {
+  return (
+    <>
+    <div className="board-row">
+        <Square  />
+        <Square />
+        <Square />
+    </div>
+    <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+    </div>
+    <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+    </div>
+    </>
+  )
+}
+
+export default Board
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,25 +1,7 @@
 import React from 'react'
 
 const Square = () => {
-    return (
-        <>
-            <div className="board-row">
-                <button className="square">1</button>
-                <button className="square">2</button>
-                <button className="square">3</button>
-            </div>
-            <div className="board-row">
-                <button className="square">4</button>
-                <button className="square">5</button>
-                <button className="square">6</button>
-            </div>
-            <div className="board-row">
-                <button className="square">7</button>
-                <button className="square">8</button>
-                <button className="square">9</button>
-            </div>
-        </>
-    )
+    return <button className="square">1</button>
 }
 
 export default Square
