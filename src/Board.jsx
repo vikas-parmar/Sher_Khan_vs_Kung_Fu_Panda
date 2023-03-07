88f6243dd7f5f731e86cc00915a2f5a9bb4a081c
Vikas Panwar <vkparmar003@gmail.com>
2023-03-07 19:40:24 +0530

    removed value prop

--- src/Board.jsx
+++ src/Board.jsx
@@ -5,19 +5,19 @@ const Board = () => {
   return (
     <>
     <div className="board-row">
-        <Square value={1} />
-        <Square value={2} />
-        <Square value={3} />
+        <Square />
+        <Square />
+        <Square />
     </div>
     <div className="board-row">
-        <Square value={4} />
-        <Square value={5} />
-        <Square value={6} />
+        <Square />
+        <Square />
+        <Square />
     </div>
     <div className="board-row">
-        <Square value={7} />
-        <Square value={8} />
-        <Square value={9} />
+        <Square />
+        <Square />
+        <Square />
     </div>
     </>
   )
