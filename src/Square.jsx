c9b5f5ec400e23c6ffe8c88d8c4ba17d6fd80cf4
Vikas Panwar <vkparmar003@gmail.com>
2023-03-07 19:41:01 +0530

    added useState & removed value prop

--- src/Square.jsx
+++ src/Square.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Square = ({value}) => {
+const Square = () => {
+    const [value, setValue] = useState(null);
     
     const handleClick =()=>{
         console.log('clicked!')
