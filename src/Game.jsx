488bffa5c112cf5233082c75e5fa966c0cb1081d
Vikas Panwar <vkparmar003@gmail.com>
2023-03-09 19:57:11 +0530

    final cleanup

--- src/Game.jsx
+++ src/Game.jsx
@@ -2,10 +2,9 @@ import { useState } from "react";
 import Board from "./Board"
 
 const Game = () => {
-    const [xIsNext, setXIsNext] = useState(true);
     const [history, setHistory] = useState([Array(9).fill(null)]);
     const [currentMove, setCurrentMove ] = useState(0);
-
+    const xIsNext = currentMove % 2 === 0;
 
     const currentSquares = history[currentMove];
 
@@ -13,11 +12,9 @@ const Game = () => {
         const nextHistory = [...history.slice(0, currentMove + 1), nextSqares];
         setHistory(nextHistory);
         setCurrentMove(nextHistory.length - 1);
-        setXIsNext(!xIsNext);
     }
     const jumpTo=(nextMove)=>{
         setCurrentMove(nextMove);
-        setXIsNext(nextMove % 2 === 0);
     }
     const moves = history.map((squares, move)=>{
         // let description;
