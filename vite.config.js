25af9d2c07adfaee08668f4dca0c5916b85c9ce7
Vikas Panwar <vkparmar003@gmail.com>
2023-03-15 18:05:37 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: [
+        /^node:.*/,
+      ],
+    },
+  },
+});
