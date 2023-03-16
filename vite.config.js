8cfbaccc75a3ec742655fd390f1bd9b870cb0429
Vikas Panwar <vkparmar003@gmail.com>
2023-03-16 12:44:22 +0530

    refractored file name main jsx

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import rollupPlugin from 'rollup-plugin-feature';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), rollupPlugin()],
+  plugins: [react()],
 });
