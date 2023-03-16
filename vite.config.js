159a314556e7db1f2d349aa9282bfabbec36e82c
Vikas Panwar <vkparmar003@gmail.com>
2023-03-16 12:28:35 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        /^node:.*/,
+      ],
+    },
+  },
 });
