e4cd870c56bc4d22121a8a32b57f731c428f7813
Vikas Panwar <vkparmar003@gmail.com>
2023-03-18 00:24:56 +0530

    debug code.

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Square from './Square';
 
+const boardRows = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
 const calculateWinner = (squares) => {
   const lines = [
     [0, 1, 2],
@@ -13,14 +19,16 @@ const calculateWinner = (squares) => {
     [2, 4, 6],
   ];
 
+  let winner = null;
+
   lines.forEach(line => {
     const [a, b, c] = line;
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      winner = squares[a];
     }
-
-    return null;
   });
+
+  return winner;
 };
 
 const Board = ({ xIsNext, squares, onPlay }) => {
@@ -33,7 +41,6 @@ const Board = ({ xIsNext, squares, onPlay }) => {
     }
 
     const nextSquares = [...squares];
-
     nextSquares[i] = xIsNext ? 'X' : 'O';
     onPlay(nextSquares);
   };
@@ -42,21 +49,17 @@ const Board = ({ xIsNext, squares, onPlay }) => {
     <div className="board">
       <h1> Tic Tac Toe Game </h1>
       <h2>{status}</h2>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {
+        boardRows.map(row => (
+          <div className="board-row" key={row}>
+            {
+              row.map(s => (
+                <Square key={s} value={squares[s]} onSquareClick={() => handleClick(s)} />
+              ))
+            }
+          </div>
+        ))
+      }
     </div>
   );
 };
