065fb54722cdfa36e06b67fd283505579d2748e8
Vikas Panwar <vkparmar003@gmail.com>
2023-03-18 12:11:49 +0530

    Deleted App Component

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Game from './Game';
-import './style.css';
-
-const App = () => <Game />;
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Game from './Game';
+
+import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Game />
   </React.StrictMode>,
 );
