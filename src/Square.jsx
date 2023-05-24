a310ba65039d0f705c3b016966bfceee556d1a06
Vikas Panwar <vkparmar003@gmail.com>
2023-05-24 16:12:29 +0530

    bug fix

--- src/Square.jsx
+++ src/Square.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Square = ({ value, onSquareClick }) => (
   <button type="button" className="square" onClick={onSquareClick}>
-    {value || '_'}
+    {value || null }
   </button>
 );
 
